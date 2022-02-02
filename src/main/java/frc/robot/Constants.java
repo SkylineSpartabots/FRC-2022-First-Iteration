cd2b090c22efdcf676ed39df9123ff94e926eaa2
Ddg3 <zachlawrence2003@hotmail.com>
2022-02-01 18:36:25 -0800

    moved new drive constants to chronosdriveconstants subclass under constants for testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -78,13 +78,48 @@ public final class Constants {
         public static final double kaVoltSecondsSquaredPerMeter = 0.15;
     
         public static final double kMaxSpeedMetersPerSecond = 3;
-
         
         public static final double DriveMaxAccelerationPerPeriodic =  20.0 / 200.0 ; //max acceleration, then, divide by 200 for 200 times per second
         public static final double RotationMaxAccelerationPerPeriodic =  100.0 / 200.0 ; //max acceleration, then, divide by 200 for 200 times per second
 
     }
 
+    public static final class ChronosDriveConstants {
+        /**
+         * The left-to-right distance between the drivetrain wheels
+         *
+         * Should be measured from center to center.
+         */
+        public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.4953; // FIXME Measure and set trackwidth
+        /**
+         * The front-to-back distance between the drivetrain wheels.
+         *
+         * Should be measured from center to center.
+         */
+        public static final double DRIVETRAIN_WHEELBASE_METERS = 0.4953; // FIXME Measure and set wheelbase
+    
+        public static final int DRIVETRAIN_PIGEON_ID = 0; // FIXME Set Pigeon ID
+    
+        public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 2; // FIXME Set front left module drive motor ID
+        public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 1; // FIXME Set front left module steer motor ID
+        public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9; // FIXME Set front left steer encoder ID
+        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(301.72576904296875); // FIXME Measure and set front left steer offset
+    
+        public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 8; // FIXME Set front right drive motor ID
+        public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 7; // FIXME Set front right steer motor ID
+        public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12; // FIXME Set front right steer encoder ID
+        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(265.9515380859375); // FIXME Measure and set front right steer offset
+    
+        public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4; // FIXME Set back left drive motor ID
+        public static final int BACK_LEFT_MODULE_STEER_MOTOR = 3; // FIXME Set back left steer motor ID
+        public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10; // FIXME Set back left steer encoder ID
+        public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(246.1761474609375); // FIXME Measure and set back left steer offset
+    
+        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6; // FIXME Set back right drive motor ID
+        public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5; // FIXME Set back right steer motor ID
+        public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; // FIXME Set back right steer encoder ID86
+        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(234.228515625); // FIXME Measure and set back right steer offset
+    }
     
     public static final class AutoConstants {
         public static final double kMaxSpeedMetersPerSecond = 3;
